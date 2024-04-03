7144b2b3b7cf31dff1741a028746c3d0138296a6
dmccoystephenson <dmccoystephenson@gmail.com>
2024-04-02 20:31:23 -0600

    Modified `start_dev_contianer.bat` to use %cd%

--- .devcontainer/start_dev_container.bat
+++ .devcontainer/start_dev_container.bat
@@ -1,2 +1,2 @@
 docker build . -t rpk-dev-container
-docker run -it -v $(pwd)\..\:/workspaces/RPKit rpk-dev-container /bin/bash
+docker run -it -v %cd%\..\:/workspaces/RPKit rpk-dev-container /bin/bash
